999a38016b42ef08aba1156079cf92099d33eed2
Grupo RV <rvportalmadeiras@gmail.com>
2026-05-18 17:55:16 -0300

    Update mdf.js

--- abas/mdf.js
+++ abas/mdf.js
@@ -30,8 +30,7 @@ class MDFManager {
 
   renderizarInterface() {
     this.container.innerHTML = `
-      <!--<div class="flex flex-col h-full">-->
-      <div class="flex items-center gap-2 h-full">
+      <div class="flex flex-col h-full">      
        <div class="flex items-center gap-2 ml-auto bg-white/80 backdrop-blur-sm p-1 rounded-2xl shadow-md border border-emerald-100">
         <button data-subaba="projetos" class="subaba-mdf-btn relative flex-1 md:flex-none px-6 py-2.5 rounded-xl font-bold text-sm transition-all duration-200 flex items-center justify-center gap-2">
             <i data-lucide="box" class="w-4 h-4"></i> 
